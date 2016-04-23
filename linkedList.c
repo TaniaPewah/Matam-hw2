9d46bfc31b31e654046c5bf942a3fa3e7f07c2e7
Tania <drob.tania@gmail.com>
2016-04-23 19:07:26 +0300

    Initial commit

---
+++ linkedList.c
@@ -0,0 +1,21 @@
+/*
+ * linkedList.c
+ *
+ *  Created on: Apr 23, 2016
+ *      Author: Tania
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+
+	printf( "yay" );
+
+
+
+	return 0;
+}
+
